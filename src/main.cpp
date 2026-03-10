0a5451c82820a52ead96429c6280e24f645f7742
Lormited <ztrtommy0529@gmail.com>
2026-03-10 20:08:32 +0800

    qrcodegen 和 FFmpeg 做的图片二进制转二维码编码器

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "encode.h"
+
+
+int main(int argc, char *argv[])
+{
+    // min_qr_video <input_binary_image> <output_video> [duration_ms]
+    if (argc <= 3)
+    {
+        std::cout << "Usage: min_qr_video <input_binary_image> <output_video> [duration_ms]\n"
+                  << "Example: min_qr_video demo_input.ppm output/min_qr.mp4 1000\n";
+        return 1;
+    }
+
+    std::string inputFile = argv[1];
+    std::string outputVideo = argv[2];
+    int durationMs = stoi(argv[3]);
+
+    QrVideoEncoder encoder;
+    if (!encoder.Init(inputFile, outputVideo, durationMs))
+        return 2;
+    encoder.DisplayBandwidthReport();
+    return encoder.Encode();
+}
